0d14d7022c8d6605366e26014038bfe56c0ca513
Dhiraj Naik <naikdhiraj9@gmail.com>
2024-01-16 23:39:29 +0530

    added on hover animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,15 @@ import React from "react";
 const Header = () => {
   return (
     <div className="flex justify-between h-full">
-      <div className="text-white">World Through my Code</div>
+      <div class="group relative w-full">
+        <p class="text-lg absolute top-0 left-0 transition-opacity duration-300 opacity-100 group-hover:opacity-0">
+          World Through my Code
+        </p>
+        <p class="text-lg absolute top-0 left-0 transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+          Dhiraj Naik
+        </p>
+      </div>
+
       <nav>
         <ul className="flex justify-between">
           <li>Work</li>
