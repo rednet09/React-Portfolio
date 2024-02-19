489d1a9cab7462987aa7bb3285b9d5df87db4769
Dhiraj Naik <naikdhiraj9@gmail.com>
2024-02-19 23:10:40 +0530

    added description

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 function App() {
   return (
-    <div className="bg-slate-900 h-full">
+    <div className="bg-slate-900 h-full flex flex-col md:flex-row">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="text-slate-200 mt-10">
+      I'm a Software Developer specializing in Web Development with almost 2
+      years of professional experience Tech stack: - Frontend: React Js, HTML,
+      CSS, Javascript, Bootstrap, Jquery, TailwindCSS. - Tools: Git, Github,
+      Figma, Canva, vs code. - Backend: Nodejs, Expressjs, MongoDB, SQl.
+    </div>
+  );
+};
+
+export default Main;
