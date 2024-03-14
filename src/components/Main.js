f8df539a444bbaa75a8d218067559fe94974de5f
Dhiraj Naik <naikdhiraj9@gmail.com>
2024-03-14 14:13:51 +0530

    fixed bugs

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,22 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div className="text-slate-200 mt-10">
-      I'm a Software Developer specializing in Web Development with almost 2
-      years of professional experience Tech stack: - Frontend: React Js, HTML,
-      CSS, Javascript, Bootstrap, Jquery, TailwindCSS. - Tools: Git, Github,
-      Figma, Canva, vs code. - Backend: Nodejs, Expressjs, MongoDB, SQl.
-    </div>
+    <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+      <div
+        id="#About"
+        className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"
+      >
+        <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+          About
+        </h2>
+      </div>
+      <div className="text-slate-200 mt-10">
+        I'm a Software Developer specializing in Web Development with almost 2
+        years of professional experience Tech stack: - Frontend: React Js, HTML,
+        CSS, Javascript, Bootstrap, Jquery, TailwindCSS. - Tools: Git, Github,
+        Figma, Canva, vs code. - Backend: Nodejs, Expressjs, MongoDB, SQl.
+      </div>
+    </section>
   );
 };
 
